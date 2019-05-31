df1980fd07683c53b7f7f671e33938b5d2babf4b
danielle <danielle@stackery.io>
2019-05-31 14:25:08 -0700

    Stackery local invoke settings

--- src/ImageProcessor/index.js
+++ src/ImageProcessor/index.js
@@ -2,5 +2,5 @@ exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  return { it: 'works' };
 };
