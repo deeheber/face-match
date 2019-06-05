18fb264ad37979050564ae156069799bdbad3ae9
danielle <danielle@stackery.io>
2019-06-05 09:33:00 -0700

    S3 event object shape

--- src/ImageProcessor/index.js
+++ src/ImageProcessor/index.js
@@ -2,5 +2,45 @@ exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
+  // S3 PUT event
+  // {
+  //   "Records": [
+  //       {
+  //           "eventVersion": "2.1",
+  //           "eventSource": "aws:s3",
+  //           "awsRegion": "us-west-2",
+  //           "eventTime": "2019-06-05T16:27:27.768Z",
+  //           "eventName": "ObjectCreated:Put",
+  //           "userIdentity": {
+  //               "principalId": "AWS:AROAIBUCOS26DRUW7IUXM:cli"
+  //           },
+  //           "requestParameters": {
+  //               "sourceIPAddress": "107.1.126.154"
+  //           },
+  //           "responseElements": {
+  //               "x-amz-request-id": "A960ADFFFF4E46F0",
+  //               "x-amz-id-2": "2IVd+/Q6LEey6DRBiFGnSYE1ksDRxujRBjkfQtdEeSgFWEW4icsEfTLCqtKtb38ZOYgqFRiK1i4="
+  //           },
+  //           "s3": {
+  //               "s3SchemaVersion": "1.0",
+  //               "configurationId": "484beff1-20d6-4795-87b3-c100d897cef3",
+  //               "bucket": {
+  //                   "name": "image-detector-development-images-053662045684",
+  //                   "ownerIdentity": {
+  //                       "principalId": "A3MG3PEC75SLNU"
+  //                   },
+  //                   "arn": "arn:aws:s3:::image-detector-development-images-053662045684"
+  //               },
+  //               "object": {
+  //                   "key": "Amazon-API-Gateway%404x.png",
+  //                   "size": 15177,
+  //                   "eTag": "d6be6e161cc99d04753ff976f572da9f",
+  //                   "sequencer": "005CF7ED6F9E77883C"
+  //               }
+  //           }
+  //       }
+  //   ]
+  // }
+
   return { it: 'works' };
 };
