9624a01029b14bc693465179fdc895353be76b9a
danielle <danielle@stackery.io>
2019-06-07 14:31:53 -0700

    Add plan for AddToCollection function

--- src/AddToCollection/index.js
+++ src/AddToCollection/index.js
@@ -1,3 +1,15 @@
+// TODO:
+// If adding an image to the bucket (ObjectCreated:Put)
+//   Grab the bucket name and object key
+//   Call indexFaces
+//   Get the FaceId from the Response
+//   Put the bucket name, object key, and FaceId in a dynamoDB table
+
+// If deleting image from bucket (ObjectRemoved:Delete)
+//   Query the table for the bucket name / object key to get the FaceId
+//   Call deleteFaces
+//   Delete the row in dynamoDB
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
