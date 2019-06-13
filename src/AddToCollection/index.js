b2bbb43985daab38aa4a88549594465aa3780ed9
danielle <danielle@stackery.io>
2019-06-12 17:21:24 -0700

    Add image metadata to dynamoDB table

--- src/AddToCollection/index.js
+++ src/AddToCollection/index.js
@@ -10,9 +10,58 @@
 //   Call deleteFaces
 //   Delete the row in dynamoDB
 
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  const BucketName = event.Records[0].s3.bucket.name;
+  const ObjectKey = event.Records[0].s3.object.key;
+  const TableName = process.env.TABLE_NAME;
+
+  let response;
+  let statusCode;
+
+  try {
+    // Adding an image to the collection
+    if (event.Records[0].eventName === 'ObjectCreated:Put') {
+      // Change this to an await from rekognition to get FaceId
+      console.log(`Adding image to Danielle collection`);
+      const FaceId = '1234';
+      console.log(`Face successfully added to Danielle collection`);
+
+      console.log(`Adding metadata to ${TableName}`);
+      const addToDDB = {
+        TableName,
+        Item: {
+          FaceId,
+          BucketName,
+          ObjectKey
+        }
+      };
+
+      await dynamodb.put(addToDDB).promise();
+      console.log(`Image metadata successfully written to dynamoDB`);
+
+      response = 'Image successfully added to collection';
+      statusCode = 200;
+    }
+
+    // Removing an image from the collection
+    if (event.Records[0].eventName === 'ObjectRemoved:Delete') {
+      console.log('delete event');
+    }
+  } catch (err) {
+    console.log('An error occured ', JSON.stringify(err, undefined, 2));
+    response = err.message;
+    statusCode = err.statusCode || 500;
+  }
+
+  return {
+    headers: {},
+    statusCode,
+    body: JSON.stringify(response)
+  };
 };
