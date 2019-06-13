35ee829277c7bc747ccda2bb730379fb42752cf5
danielle <danielle@stackery.io>
2019-06-13 11:17:22 -0700

    Add/remove data from dynamoDB

--- src/AddToCollection/index.js
+++ src/AddToCollection/index.js
@@ -27,32 +27,50 @@ exports.handler = async (event, context) => {
   try {
     // Adding an image to the collection
     if (event.Records[0].eventName === 'ObjectCreated:Put') {
-      // Change this to an await from rekognition to get FaceId
       console.log(`Adding image to Danielle collection`);
-      const FaceId = '1234';
+      // TODO add call to rekognition indexFaces here and grab FaceId from reply
+      const FaceId = '8675309';
       console.log(`Face successfully added to Danielle collection`);
 
       console.log(`Adding metadata to ${TableName}`);
-      const addToDDB = {
+      await dynamodb.put({
         TableName,
         Item: {
           FaceId,
           BucketName,
           ObjectKey
         }
-      };
-
-      await dynamodb.put(addToDDB).promise();
+      }).promise();
       console.log(`Image metadata successfully written to dynamoDB`);
 
       response = 'Image successfully added to collection';
-      statusCode = 200;
     }
 
     // Removing an image from the collection
     if (event.Records[0].eventName === 'ObjectRemoved:Delete') {
-      console.log('delete event');
+      console.log('Getting FaceId from dynamoDB');
+      const { Item } = await dynamodb.get({
+        TableName,
+        Key: { ObjectKey }
+      }).promise();
+      console.log('FaceId successfully obtained from dynamoDB');
+
+      console.log('Deteting face from the Danielle collection');
+      // TODO add deleteFaces call to rekognition here
+      console.log(`FacedId is ${Item.FaceId}`);
+      console.log('Face successfully deleted from the Danielle collection');
+
+      console.log('Removing image metadata from dynamoDB');
+      await dynamodb.delete({
+        TableName,
+        Key: { ObjectKey }
+      }).promise();
+      console.log('Image metadata successfully removed from dynamoDB');
+
+      response = 'Image successfully removed from collection';
     }
+
+    statusCode = 200;
   } catch (err) {
     console.log('An error occured ', JSON.stringify(err, undefined, 2));
     response = err.message;
