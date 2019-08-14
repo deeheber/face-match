67ae528070f666cce6966670dda547b2e9dbca90
danielle <danielle@stackery.io>
2019-08-14 09:26:50 -0700

    Setup cf custom resource replies to avoid forever deployments

--- src/CreateCollection/index.js
+++ src/CreateCollection/index.js
@@ -1,6 +1,17 @@
-exports.handler = async (event, context) => {
+const { sendSuccess, sendFailure } = require('cfn-custom-resource');
+
+exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
+  // TODO:
+  // Pull down the collectionId environment variable from the parameter store https://docs.aws.amazon.com/systems-manager/latest/APIReference/API_GetParameter.html
+  // Throw error if the env var does not exist
+  // Check to see if the collection exists https://docs.aws.amazon.com/rekognition/latest/dg/list-collection-procedure.html
+  // Create the collection if it doesn't https://docs.aws.amazon.com/rekognition/latest/dg/create-collection-procedure.html
 
-  return {};
+  try {
+    await sendSuccess('CreateCollection', {}, event);
+  } catch (err) {
+    await sendFailure(err.message, event);
+  }
 };
