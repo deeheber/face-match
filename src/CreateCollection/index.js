f7897e64a3eaf6e45e821c8993169f6eed2b29b7
danielle <danielle@stackery.io>
2019-08-14 11:11:37 -0700

    Finish up CreateCollection function.

--- src/CreateCollection/index.js
+++ src/CreateCollection/index.js
@@ -1,16 +1,28 @@
 const AWS = require('aws-sdk');
+const rekognition = new AWS.Rekognition();
 const { sendSuccess, sendFailure } = require('cfn-custom-resource');
 
 exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
-  // TODO:
-  // Pull down the collectionId environment variable from the parameter store https://docs.aws.amazon.com/systems-manager/latest/APIReference/API_GetParameter.html
-  // Throw error if the env var does not exist
-  // Check to see if the collection exists https://docs.aws.amazon.com/rekognition/latest/dg/list-collection-procedure.html
-  // Create the collection if it doesn't https://docs.aws.amazon.com/rekognition/latest/dg/create-collection-procedure.html
+
   try {
-    console.log('COLLECTION ID: ', process.env.COLLECTION_NAME);
+    const CollectionId = process.env.COLLECTION_NAME || '';
+    console.log('COLLECTION ID ENV VAR: ', process.env.COLLECTION_NAME);
+
+    if (CollectionId === '') {
+      throw new Error('Collection id env parameter does not exist');
+    }
+
+    const { CollectionIds } = await rekognition.listCollections({}).promise();
+    console.log('EXISTING COLLECTIONS: ', CollectionIds);
+
+    // Create the collection if it does not exist
+    if (!CollectionIds.includes(CollectionId)) {
+      console.log(`Creating new collection: ${CollectionId}`);
+      await rekognition.createCollection({ CollectionId }).promise();
+      console.log('New collection successfully created.');
+    }
     await sendSuccess('CreateCollection', {}, event);
   } catch (err) {
     console.error(err.message, err);
